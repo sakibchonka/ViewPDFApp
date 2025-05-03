32196d6570e3c7b5251e54644a057831f488ebc7
Mohammed Saqib <mohammeds@adobe.com>
2025-05-03 15:28:28 +0530

    redirects to loginView after sign out

--- ViewPDF/View/MainView.swift
+++ ViewPDF/View/MainView.swift
@@ -14,33 +14,41 @@ enum Destination: Hashable {
 struct MainView: View {
 	
 	@StateObject private var downloader = PDFDownloader()
+	@StateObject private var authVM = AuthViewModel()
 	@State private var path: [Destination] = []
 	
 	var body: some View {
-		NavigationStack(path: $path) {
-			VStack(spacing: 20) {
-				Text("Welcome")
-				
-				Button("Open PDF") {
-					downloader.fetch(from: "https://fssservices.bookxpert.co/GeneratedPDF/Companies/nadc/2024-2025/BalanceSheet.pdf")
-				}
-
-				
-				if let error = downloader.errorMessage {
-					Text("\(error)").foregroundStyle(.red)
-				}
-			}
-			.padding()
-			.navigationDestination(for: Destination.self) { destination in
-				switch destination {
-				case .pdfViewer(let url):
+		if authVM.isSignedIn {
+			NavigationStack(path: $path) {
+				VStack(spacing: 20) {
+					Text("Welcome")
 					
-					PDFKitView(url: url)
+					Button("Open PDF") {
+						downloader.fetch(from: "https://fssservices.bookxpert.co/GeneratedPDF/Companies/nadc/2024-2025/BalanceSheet.pdf")
+					}
+					
+					Button("sign out") {
+						authVM.signOut()
+					}
+					
+					if let error = downloader.errorMessage {
+						Text("\(error)").foregroundStyle(.red)
+					}
+				}
+				.padding()
+				.navigationDestination(for: Destination.self) { destination in
+					switch destination {
+					case .pdfViewer(let url):
+						
+						PDFKitView(url: url)
+					}
+				}
+				.onReceive(downloader.$downloadedURL.compactMap{$0}) { url in
+					path.append(.pdfViewer(url))
 				}
 			}
-			.onReceive(downloader.$downloadedURL.compactMap{$0}) { url in
-				path.append(.pdfViewer(url))
-			}
+		} else {
+			LoginView()
 		}
 	}
 }
